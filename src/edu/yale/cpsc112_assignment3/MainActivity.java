ffd0239f2559a6284bcceb05da3a4fe454aa13b9
AnthonyAmadeo <anthony.amadeo@yale.edu>
2014-11-16 22:13:47 -0500

    Added Text Message Sender
    I rearranged the order of the toast and the sendSms. The video online
    had it in that order, and I think it makes sense to have the app send
    the text before we let them know that it worked.

--- src/edu/yale/cpsc112_assignment3/MainActivity.java
+++ src/edu/yale/cpsc112_assignment3/MainActivity.java
@@ -2,6 +2,7 @@ package edu.yale.cpsc112_assignment3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -38,21 +39,31 @@ public class MainActivity extends Activity {
 		    	    Toast.makeText(MainActivity.this, "ERROR: Please select at least one recipient.", Toast.LENGTH_LONG).show();
 		    	}
 		    		else{
-		    		Toast.makeText(MainActivity.this, "Submitted!",Toast.LENGTH_LONG).show();
 		    		// Put texting code HERE
+		    		sendSms(Recipient, Amount);
+		    		Toast.makeText(MainActivity.this, "Submitted!",Toast.LENGTH_LONG).show();
 			}
 		    	 }});
         }  	
    
 
 
-    @Override
+    protected void sendSms(String Recipient, String Amount) {
+		// TODO Auto-generated method stub
+		SmsManager manager = SmsManager.getDefault();
+		manager.sendTextMessage(Recipient, null, Amount, null, null);
+	}
+
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+<<<<<<< Updated upstream
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -64,4 +75,21 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+=======
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item)
+   {
+      // Handle action bar item clicks here. The action bar will
+      // automatically handle clicks on the Home/Up button, so long
+      // as you specify a parent activity in AndroidManifest.xml.
+      int id = item.getItemId();
+      if (id == R.id.action_settings)
+      {
+         return true;
+      }
+      return super.onOptionsItemSelected(item);
+   }
+   
+   
+>>>>>>> Stashed changes
 }
