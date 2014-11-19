26a0a067f88a94efeb2ef26550b420febc5afde4
Andrew Schmidt <andrew.schmidt@yale.edu>
2014-11-19 01:16:23 -0500

    Added Up to ActionBar
    Now you can go back to the main page from settings.

--- src/edu/yale/cpsc112_assignment3/MainActivity2.java
+++ src/edu/yale/cpsc112_assignment3/MainActivity2.java
@@ -11,6 +11,8 @@ public class MainActivity2 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings_main);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
